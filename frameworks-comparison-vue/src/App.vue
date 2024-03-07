845d521bae8240bc8a1b8d01019af93f4ff3ebcc
Adam <adam.kowalczuk@poczta.fm>
2024-03-07 20:54:19 +0100

    Configure Vue project

--- frameworks-comparison-vue/src/App.vue
+++ frameworks-comparison-vue/src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="app">
-    App
+    <LeftSidebar />
+    <router-view/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+import LeftSidebar from './components/LeftSidebar.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components:{
+    LeftSidebar
+  },
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+
+
+
+    return {};
+  }
 });
 </script>
-
-<style>
-
-</style>
---
+++ frameworks-comparison-vue/src/layouts/AuthLayout/AuthLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="!isLoggedIn" class="auth-layout">
+    <router-view/>
+    <div class="auth-image">
+      <img src="../../assets/images/auth-image.png" alt="logo" />
+    </div>
+  </div>
+  <router-view v-else />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  name: 'AuthLayout',
+  computed: {
+    isLoggedIn() {
+      return useStore().state.isLoggedIn;
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss">
+@import "src/assets/scss/variables.scss";
+
+.auth-layout {
+  display: flex;
+  padding-top: 2.5rem;
+  padding-bottom: 2.5rem;
+  justify-content: center;
+  align-items: center;
+  width: -webkit-fill-available;
+}
+
+.auth-image {
+  width: 100%;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  img {
+    max-width: 500px;
+    height: fit-content;
+  }
+}
+
+</style>
