7d0d190aab4843670e39cf33e53af0706ae65482
Adam <adam.kowalczuk@poczta.fm>
2024-03-19 18:53:51 +0100

    Add userService

---
+++ frameworks-comparison-vue/src/services/userService.ts
@@ -0,0 +1,72 @@
+import axios from "axios";
+import type { IUser } from "@/types";
+import { getToken } from "@/utils/authUtils";
+
+const getUsers = () => {
+  const token = getToken();
+  if (!token) {
+    return Promise.reject("Token not found in localStorage");
+  }
+  return axios
+    .get(`${process.env.VUE_APP_API_URL}/api/users`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+const getUserById = (userId: string) => {
+  const token = getToken();
+  if (!token) {
+    return Promise.reject("Token not found in localStorage");
+  }
+  return axios
+    .get(`${process.env.VUE_APP_API_URL}/api/users/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+interface EditUserData {
+  userId: string;
+  userData: IUser;
+}
+
+const editUser = ({ userId, userData }: EditUserData): Promise<IUser[]> => {
+  const token = getToken();
+  if (!token) {
+    return Promise.reject("Token not found in localStorage");
+  }
+  return axios
+    .put(`${process.env.VUE_APP_API_URL}/api/users/${userId}`, userData, {
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export default {
+  getUsers,
+  getUserById,
+  editUser,
+};
