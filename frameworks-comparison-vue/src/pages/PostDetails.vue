23909b8522ffae9e36b2d193538a0a4c3254e79f
Adam <adam.kowalczuk@poczta.fm>
2024-04-08 20:40:03 +0200

    Add PostDetails page

--- frameworks-comparison-vue/src/pages/PostDetails.vue
+++ frameworks-comparison-vue/src/pages/PostDetails.vue
@@ -1,11 +1,57 @@
-<script setup lang="ts">
+<script>
+import PostCard from "@/components/PostCard.vue";
+import PostService from "@/services/postService";
+import Loader from "@/components/Loader.vue";
+import BackIcon from "@/assets/icons/back.svg";
+
+export default {
+  components: {
+    PostCard,
+    Loader,
+  },
+  data() {
+    return {
+      postId: null,
+      post: null,
+      isPostLoading: true,
+      BackIcon: BackIcon,
+    };
+  },
+  methods: {
+    handleGetPost() {
+      if (this.postId) {
+        PostService.getPostById(this.postId)
+          .then((response) => {
+            this.post = response.post;
+            this.isPostLoading = false;
+          })
+          .catch((error) => {
+            console.error("Error fetching posts:", error);
+          });
+      }
+    },
+    navigateBack() {
+      this.$router.go(-1);
+    },
+  },
+  mounted() {
+    this.postId = this.$route.params.id;
+    this.handleGetPost();
+  },
+};
 </script>
 
 <template>
-  <div>
-    <h1>PostDetails</h1>
+  <div class="right-container">
+    <div class="title-wrapper">
+      <img :src="BackIcon" alt="back" style="cursor: pointer" @click="navigateBack" class="title-wrapper-img" />
+      <h4>Back</h4>
+    </div>
+    <div v-if="isPostLoading && !post">
+      <Loader />
+    </div>
+    <div v-else>
+      <PostCard :post="post" :getPosts="handleGetPost" />
+    </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-</style>
